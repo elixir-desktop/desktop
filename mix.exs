9cd28e80e1fd9599fb9774632918e0758c632c2a
Mihai Potra <mike@mpotra.com>
2021-10-14 09:50:29 +0300

    Point ex_sni dependency to hex

--- mix.exs
+++ mix.exs
@@ -62,7 +62,7 @@ defmodule Desktop.MixProject do
       {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
 
       # DBus SNI
-      {:ex_sni, github: "elixir-desktop/ex_sni"},
+      {:ex_sni, "~> 0.1.0"},
 
       # Phoenix & Plug
       {:plug, "> 1.0.0"},
