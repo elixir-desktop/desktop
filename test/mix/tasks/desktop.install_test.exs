e2fe3be58392610b07b83f763835b1b223e41d3f
Adam Wight <adam.wight@wikimedia.de>
2025-10-30 06:16:04 -0700

    Update tests

--- test/mix/tasks/desktop.install_test.exs
+++ test/mix/tasks/desktop.install_test.exs
@@ -9,19 +9,20 @@ defmodule Mix.Tasks.Desktop.InstallTest do
       + | {:desktop,
     """)
     |> assert_has_patch("lib/my_app/application.ex", """
-      - | MyAppWeb.Endpoint
-      + | MyAppWeb.Endpoint,
-      + | {Desktop.Window,
-      + |  [
-      + |    app: :my_app,
-      + |    id: MyApp.MainWindow,
-      + |    title: "my_app",
-      + |    size: {600, 500},
-      + |    icon: "icon.png",
-      + |    menubar: MyApp.MenuBar,
-      + |    icon_menu: MyApp.Menu,
-      + |    url: {MyAppWeb.Endpoint, :url, []}
-      + |  ]}
+     - | MyAppWeb.Endpoint
+     + | MyAppWeb.Endpoint,
+     + | {Desktop.Window,
+     + |  [
+     + |    app: :my_app,
+     + |    id: MyApp.MainWindow,
+     + |    title: "my_app",
+     + |    size: {600, 500},
+     + |    menubar: MyApp.MenuBar,
+     + |    icon_menu: MyApp.Menu,
+     + |    url: &MyAppWeb.Endpoint.url/0
+     + |  ]}
     """)
+    |> Igniter.Test.assert_creates("lib/my_app/menu.ex")
+    |> Igniter.Test.assert_creates("lib/my_app/menu_bar.ex")
   end
 end
